86038f88d83b58154df0ec4f779f984a181b5f82
Aire-One <erwan.maugere@gmail.com>
2018-08-13 15:43:06 +0200

    Add source file header to `init.lua`

--- init.lua
+++ init.lua
@@ -1,6 +1,12 @@
----
----
----
+-----------------
+-- awesome-AnimationFramework init.lua
+-- A Simple Animation Framework for the Awesome WM.
+--
+-- Copyright (C) 2018 Aire-One
+--
+-- Author : Aire-One (Aire-One@github.com ; Aire-One@gitlab.com)
+-----------------
+
 return {
     Animation = require('awesome-AnimationFramework.Animation'),
     AnimatedObject = require('awesome-AnimationFramework.AnimatedObject')
