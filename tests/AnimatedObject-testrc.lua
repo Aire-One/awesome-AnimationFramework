55497ba1e3700d87f0a4ed42d54e84488e85446c
Aire-One <erwan.maugere@gmail.com>
2019-09-26 17:01:51 +0200

    modernize `tests/AnimatedObject.lua`

--- tests/AnimatedObject-testrc.lua
+++ tests/AnimatedObject-testrc.lua
@@ -27,51 +27,50 @@
 
 -- require user rc.lua as a base config
 -- user rc.lua file should be on a place loaded by awesomewm
-require('rc')
+require 'rc'
 
 -- load awesome wm libraries
-local wibox = require('wibox')
+local wibox = require 'wibox'
 
 -- Loading the library to test it
 -- again, the file should be placed in a directory loaded by awesomewm
-local AnimObj = require('awesome-AnimationFramework/AnimatedObject')
+local AnimatedObject = require 'awesome-AnimationFramework.AnimatedObject'
 
-local animationWidget = wibox ({
+local wib = wibox {
     ontop = true,
     visible = true,
-    opacity = 1,
     x = 50,
     y = 50,
     width = 100,
     height = 100
-})
+}
 
--- wid is the object we will use now
-local wid = AnimObj(animationWidget)
+-- animatedWibox is the object we will use now
+local animatedWibox = AnimatedObject(wib)
 
--- wid contains your wibox and you still can access it:
-wid.subject.width = 300
+-- animatedWibox contains your wibox and you still can access it:
+animatedWibox.subject.width = 300
 -- you also can access to all the animations registered for you widget:
-print('There are currently ' .. #wid.anims
+print('There are currently ' .. #animatedWibox.anims
     .. ' animations registered.')
 
-wid:connect_signal('anim::animation_finished', function (s)
+animatedWibox:connect_signal('anim::animation_finished', function (s)
     print(s, 'anim finished')
 end)
 
-animationWidget:connect_signal('mouse::enter', function ()
+wib:connect_signal('mouse::enter', function ()
     -- clear all the current animations on wid
-    wid:clearAnimations()
+    animatedWibox:clearAnimations()
 
     -- Register a new animation
-    wid:register_animation {
+    animatedWibox:register_animation {
         target = { width = 300 },
         easing = 'inOutCubic',
         duration = 0.325
     }
 
     -- you can also get back the Animation object...
-    local animH = wid:register_animation {
+    local animH = animatedWibox:register_animation {
         target = { height = 300 },
         easing = 'inOutCubic',
         duration = 0.25
@@ -85,23 +84,23 @@ animationWidget:connect_signal('mouse::enter', function ()
         function (s) print('anim stared', s) end)
 
     -- start all registered animations:
-    wid:startAnimations()
+    animatedWibox:startAnimations()
 end)
 
-animationWidget:connect_signal('mouse::leave', function ()
-    wid:clearAnimations()
+wib:connect_signal('mouse::leave', function ()
+    animatedWibox:clearAnimations()
 
-    wid:register_animation {
+    animatedWibox:register_animation {
         target = { width = 100 },
         easing = 'inOutCubic',
         duration = 0.25,
         delay = 0.075
     }
-    wid:register_animation {
+    animatedWibox:register_animation {
         target = { height = 100 },
         easing = 'inOutCubic',
         duration = 0.325
     }
 
-    wid:startAnimations()
+    animatedWibox:startAnimations()
 end)
